75dadd78c71465872c0bcda85015e1dd3a675c01
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2024-09-09 09:56:08 +0530

    conditional and loops

---
+++ conditional/conditionals.js
@@ -0,0 +1,25 @@
+// if...else -->
+// let num = 6;
+// if(num == 5){
+//     console.log("Number is 5")
+// }else{
+//     console.log("Number is not 5")
+// }
+
+// nested if...else -->
+// let day = 4
+// if(day == 1){
+//     console.log("Monday")
+// }else if(day == 2){
+//     console.log("Tuesday")
+// }else if(day == 3){
+//     console.log("Wednesday")
+// }else {
+//     console.log("Not a day")
+// }
+
+// ternary operator
+// condition ? trueTask : falseTask;
+// let num = 6;
+// num == 5 ? console.log("Num is 5")
+//  : console.log("Num is not 5");
---
+++ loops/loops.js
@@ -0,0 +1,58 @@
+// for loops
+// while loops
+// do...while loops
+
+// for...in --> object
+// let obj = {
+//     name : "Shrey Khandelwal",
+//     univ : "Vignan",
+//     year : 2024,
+//     batch : "4H"
+// }
+// console.log(obj["name"])
+// for( key in obj){
+//     console.log(obj[key])
+// console.log(obj.key)
+// }
+
+// for...of --> arrays
+// let arr = [1,2,3, "Shrey"]
+// for(key of arr){
+//     console.log(key)
+// }
+
+// for...each
+// forEach doesn't return a value.
+// We cannot nest multiple forEach
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// printValue is considered as a callback function
+// let printValue = function (i){
+//     console.log(i)
+// }
+// arr.forEach((i)=>{
+//     console.log(i+5)
+// })
+// arr.forEach(printValue)
+
+// map
+// let modifiedArray = arr
+//   .map((i) => {
+//     return i + 5;
+//   })
+//   .map((j) => {
+//     return j*10;
+//   });
+// console.log(arr)
+// console.log(modifiedArray)
+
+// filter
+console.log(arr)
+let modifiedArray = arr.map((i) => {
+    return i + 5;
+  }).filter((j)=>{
+    return j > 10
+  })
+console.log(modifiedArray)
+
+// reduce
